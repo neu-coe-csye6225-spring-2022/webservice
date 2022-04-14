c71eb5d248631555409100c1c1440e7554dd051b
Yiqing Huang <huang.yiqin@northeastern.edu>
2022-04-14 16:48:18 -0400

    feat: update webapp

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path, re_path
 
 from rest_framework import routers
-from app.views import HealthzViewSet, UserCreate, UserManage, ImageViewSet
+from app.views import HealthzViewSet, UserCreate, UserManage, ImageViewSet, UserVerify
 
 routers_healthz = routers.DefaultRouter()
 routers_healthz.register(r'healthz', HealthzViewSet)
 routers_img = routers.DefaultRouter()
-routers_img.register(r'v2/user/self/pic', ImageViewSet)
+routers_img.register(r'v1/user/self/pic', ImageViewSet)
 
 # routers_user = routers.DefaultRouter()
 # routers_user.register(r'v1/user', UserCreate)
@@ -15,6 +15,7 @@ routers_img.register(r'v2/user/self/pic', ImageViewSet)
 urlpatterns = [
     path('', include(routers_healthz.urls)),  # ip:port/healthz/
     path('', include(routers_img.urls)),
-    path('v2/user/', UserCreate.as_view()),  # ip:port/v1/user/
-    path('v2/user/self/', UserManage.as_view()),  # ip:port/v1/user/self/
+    path('v1/user/', UserCreate.as_view()),  # ip:port/v1/user/
+    path('v1/user/self/', UserManage.as_view()),  # ip:port/v1/user/self/
+    path('v1/verifyUserEmail/', UserVerify.as_view())
 ]
